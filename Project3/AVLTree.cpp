95cb363b34ed06b114377329cce3f3026deabe4a
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-02 17:31:11 -0400

    Initial functions

--- Project3/AVLTree.cpp
+++ Project3/AVLTree.cpp
@@ -1,8 +1,39 @@
 #include "stdafx.h"
 #include "AVLTree.h"
+#include <vector>
+using namespace std;	//This is bad style
 
 
 AVLTree::AVLTree(){}
 
 
 AVLTree::~AVLTree(){}
+
+
+
+
+bool AVLTree::insert(int key, int value){
+	return false;	//TODO - remove
+}
+
+
+int AVLTree::getHeight(){
+	return 0;	//TODO - remove
+}
+
+int AVLTree::getSize(){
+	return 0;	//TODO - remove
+}
+
+void AVLTree::print(){
+
+}
+
+bool AVLTree::find(int key, int& value){
+	return false;	//TODO - remove
+}
+
+vector<int> AVLTree::findRange(int lowkey, int highkey){
+	vector<int> hi;	//TODO - remove
+	return hi;	//TODO - remove
+}
--- Project3/AVLTree.h
+++ Project3/AVLTree.h
@@ -1,7 +1,18 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
 class AVLTree{
 public:
 	AVLTree();
 	~AVLTree();
+	bool insert(int key, int value);
+	int getHeight();
+	int getSize();
+	void print();
+	bool find(int key, int & value);
+	vector<int> findRange(int lowkey, int highkey);
 };
 
