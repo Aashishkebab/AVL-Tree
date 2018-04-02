ceaf8dd58d26517be8360b6725715bfc2c1c7a1f
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-02 17:49:52 -0400

    getters

--- Project3/TreeNode.cpp
+++ Project3/TreeNode.cpp
@@ -8,6 +8,14 @@ TreeNode::TreeNode(){
 TreeNode::TreeNode(int key, int value):key(key), value(value){	//This is a C++ initializer list
 }
 
+int TreeNode::getKey(){
+	return this->key;
+}
+
+int TreeNode::getValue(){
+	return this->value;
+}
+
 //The below is the same thing as above, but written in a style that doesn't use initializer lists
 //TreeNode::TreeNode(int key, int value){
 //	this->key = key;
--- Project3/TreeNode.h
+++ Project3/TreeNode.h
@@ -3,6 +3,8 @@ class TreeNode{
 public:
 	TreeNode();
 	TreeNode(int key, int value);
+	int getKey();
+	int getValue();
 	~TreeNode();
 
 private:
