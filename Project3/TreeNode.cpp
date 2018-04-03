8feb04aef107e4eabc1d71df543616505f2bdad2
Aashish Bharadwaj <aashish.bharadwaj01@gmail.com>
2018-04-03 15:04:23 -0400

    Throw stuff

--- Project3/TreeNode.cpp
+++ Project3/TreeNode.cpp
@@ -15,27 +15,51 @@ TreeNode::TreeNode(int key, int value):key(key), value(value){	//This is a C++ i
 //}
 
 int TreeNode::getKey(){
+	if(!this){
+		throw "Cannot get key of null";
+		return NULL;
+	}
 	return this->key;
 }
 
 int TreeNode::getValue(){
+	if(!this){
+		throw "Cannot get value of null";
+		return NULL;
+	}
 	return this->value;
 }
 
 TreeNode* TreeNode::getLeftChild(){
+	if(!this){
+		throw "Cannot get left child of null";
+		return nullptr;
+	}
 	return this->leftChild;
 }
 
 TreeNode* TreeNode::getRightChild(){
+	if(!this){
+		throw "Cannot get right child of null";
+		return nullptr;
+	}
 	return this->rightChild;
 }
 
 
 void TreeNode::setLeftChild(TreeNode* theNode){
+	if(!this){
+		throw "Cannot set left child of null";
+		return;
+	}
 	this->leftChild = theNode;
 }
 
 void TreeNode::setRightChild(TreeNode* theNode){
+	if(!this){
+		throw "Cannot get right child of null";
+		return;
+	}
 	this->rightChild = theNode;
 }
 
